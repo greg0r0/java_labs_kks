c86e545c69ec31bccb9fc0d4955df0b38bc6215d
greg0r0 <game_grego@mail.ru>
2020-11-08 04:26:07 +0300

    Added and refactored methods

--- lab2/lab2_linked_list/LList.java
+++ lab2/lab2_linked_list/LList.java
@@ -11,14 +11,7 @@ public class LList<T>{
     //пустой конструктор
     public LList(){};
 
-    //инициализирующий конструктор
-    public LList(LListNode<T> node){
-        list_length = 1;
-        first_element = node;
-        last_element = null;
-    }
-
-    private LList(LListNode<T> fe, LListNode<T> se, int len) {
+    public LList(LListNode<T> fe, LListNode<T> se, int len) {
         list_length   = len;
         first_element = fe;
         last_element  = se;
@@ -31,21 +24,27 @@ public class LList<T>{
     }
 
     //Задать входной элемент списка
-    public void setFirstNode(LListNode<T> node){
+    public void addFirstNode(LListNode<T> node){
         node.setNextNode(first_element);
         first_element = node;
     }
 
     //Задать выходной элемент списка
-    public void setLastNode(LListNode<T> node){
+    public void addLastNode(LListNode<T> node){
         if (list_length == 1){
             first_element.setNextNode(node); //костыль из-за last_node
             last_element = node;
         }
         last_element.setNextNode(node);
         node.setNextNode(null);
+        last_element = node;
+        list_length++;
     }
 
+    //Удалить первый элемент списка
+    public void deleteFirstNode(){
+        first_element = first_element.getNextNode();
+    }
     //удалить последний элемент списка
     public void deleteLastNode(){
         LListNode<T> node = first_element;
@@ -54,6 +53,33 @@ public class LList<T>{
             node = node.getNextNode();
         }
         node.setNextNode(null);
+    }
+
+    //Удаление элемента по значению
+    //Будет удален первый найденный элемент
+    public void deleteByValue(T value) throws Exception{
+        LListNode<T> node = first_element;
+        LListNode<T> lastNode = null;
+        
+        while (node != null){
+            if (node.getData() == value){
+
+                if (node == first_element){
+                    first_element = node.getNextNode();
+                    node.setNextNode(null);
+                } else if (node == this.last_element){
+                    lastNode.setNextNode(null);
+                } else {
+                    lastNode.setNextNode(node.getNextNode());
+                    node.setNextNode(null); 
+                }
+
+                return;
+            }
+            lastNode = node;
+            node = node.getNextNode();
+        }
+        throw new Exception("No element found for delete.");
     }
 
     //Получить количество элементов в списке
@@ -65,6 +91,7 @@ public class LList<T>{
     public void deleteList(){
         //боже, благослови Garbage Collector
         first_element = null;
+        last_element = null;
     }
 
     //DEBUG: вывод цепочки
--- lab2/main.java
+++ lab2/main.java
@@ -7,10 +7,29 @@ class Program{
     {
         LListNode<Integer> a = new LListNode<Integer>(1, null);
         LListNode<Integer> b = new LListNode<Integer>(2, null);
-        
-        LList<Integer> lst = new LList<Integer>(a);
-        lst.setLastNode(b);
-        LList<Integer> lst2 = new LList<Integer>(lst);
+        LListNode<Integer> c = new LListNode<Integer>(3, null);
+        LListNode<Integer> d = new LListNode<Integer>(4, null);
+        LListNode<Integer> e = new LListNode<Integer>(5, null);
+        LList<Integer> lst = new LList<Integer>(a, b, 2);
+        lst.addLastNode(c);
+        lst.addLastNode(d);
+        lst.addLastNode(e);
+        lst.PrintRope();
+        try {
+            lst.deleteByValue(3);
+            lst.PrintRope();
+            lst.deleteByValue(5);
+            lst.PrintRope();
+            lst.deleteByValue(1);
+            lst.PrintRope();
+
+            lst.deleteByValue(1337);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        lst.addFirstNode(c);
+        lst.PrintRope();
+        lst.deleteFirstNode();
         lst.PrintRope();
     }
 }
