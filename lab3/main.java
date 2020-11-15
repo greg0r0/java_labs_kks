50d7d4d11ef5ea766da592c0f17c0f8cd938253a
greg0r0 <game_grego@mail.ru>
2020-11-15 06:25:35 +0300

    Added tests

--- lab3/main.java
+++ lab3/main.java
@@ -6,24 +6,7 @@ import lab3_vector.*;
 
 class Program{
     public static void main(String[] args){
-        lab3_vector.Vector<Integer> vec = new lab3_vector.Vector<Integer>();
-        //System.out.println(vec.getLen());
-        for (int i = 0; i<=5; i++){
-            vec.push(i);
-            //System.out.println(vec.getLen());
-        }
-        vec.printrope();
-        try{
-            vec.insert(15, 3);
-            vec.printrope();
-        } catch (Exception ex){
-
-        }
-        try{
-            vec.delete(3);
-            vec.printrope();
-        } catch (Exception ex){
-
-        }
+        VectorTestSuite tests = new VectorTestSuite();
+        tests.Run();
     }
 }
