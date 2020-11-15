25a867f025f1ffddcafff0bf554cb8513157fb63
greg0r0 <game_grego@mail.ru>
2020-11-15 04:44:24 +0300

    Added comments

--- lab3/lab3_vector/Vector.java
+++ lab3/lab3_vector/Vector.java
@@ -3,32 +3,36 @@ package lab3_vector;
 import lab3_vector.*;
 
 public class Vector<T>{
+    //our main array
     private T[] arr;
+    //len for current array lenth
     private int len;
+    //capacity for current maximum array length
     private int capacity;
 
+    //basic constructor
     public Vector(){
         this.arr = (T[])new Object[1];
         this.capacity = 1;
         this.len = 0;
     }
-
+    //constructor with custom capacity
     public Vector(int _capacity){
         this.arr = (T[])new Object[_capacity];
         this.capacity = _capacity;
         this.len = 0;
     }
-    
+    //private constructor for copy constructor
     private Vector(T[] _arr, int _len, int _cap){
         this.arr = _arr;
         this.len = _len;
         this.capacity = _cap;
     }
-
+    //copy constructor
     public Vector(Vector<T> vec){
         this(vec.arr, vec.len, vec.capacity);
     }
-
+    //resize function for array extension
     private void resize(){
         T[] new_arr = (T[])new Object[2*this.capacity];
         for (int i = 0; i < this.capacity; i++){
@@ -45,6 +49,7 @@ public class Vector<T>{
         return this.len;
     }
 
+    //add element to end of array
     public void push(T element){
         if (this.len == this.capacity){
             resize();
@@ -52,7 +57,7 @@ public class Vector<T>{
         this.arr[len] = element;
         this.len++;
     }
-
+    //delete last element and get him
     public T pop() throws Exception{
         if (this.len == 0){
             throw new Exception("Error: nothing to pop");
@@ -60,7 +65,7 @@ public class Vector<T>{
         len--;
         return this.arr[len];
     }
-
+    //standart get element operation
     public T get(int index) throws Exception{
         if (index > this.len || index < 0){
             throw new Exception("Bad Index");
@@ -70,6 +75,7 @@ public class Vector<T>{
 
     }
 
+    //insert function without copy method using
     public void insert(T element, int index) throws Exception{
         if (index > this.len || index < 0){
             throw new Exception("Bad Index");
@@ -90,6 +96,7 @@ public class Vector<T>{
         arr[index] = element;
     }
 
+    //insert function without copy method using
     public void delete(int index) throws Exception{
         if (index > this.len || index < 0){
             throw new Exception("Bad Index");
@@ -108,6 +115,7 @@ public class Vector<T>{
         this.len = 0;
     }
 
+    //method for debug
     public void printrope(){
         for (int i = 0; i < this.len; i++)
         {
