48890bd025f4d1358ca6bfa9bcd7908828b830d3
greg0r0 <game_grego@mail.ru>
2020-12-04 15:08:34 +0300

    lab6/main cleared

--- lab6/main.java
+++ lab6/main.java
@@ -6,24 +6,6 @@ import java.util.Arrays;
 
 class Program{
 
-    public static void print_arr(Integer[] c){
-        for (int i = 0; i < c.length; i++){
-            System.out.print(c[i] + " ");
-        }
-        System.out.println();
-    }
-    private static <T extends Comparable<T>> boolean check_order(T arr[], int left, int right){
-
-        for (int i = left+1; i < right; i++){
-            if (arr[i-1].compareTo(arr[i]) > 0){
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-
     public static void main(String[] args) {
         FastSortAlgsTestSuite tests = new FastSortAlgsTestSuite();
         tests.Run();
