1fd3cfdb7e67458a8ba4aceb00908ed23ea35b25
greg0r0 <game_grego@mail.ru>
2020-12-04 18:01:58 +0300

    Added lab7 MSD sort draft

--- lab7/lab7_radix_sorts/RadixSorts.java
+++ lab7/lab7_radix_sorts/RadixSorts.java
@@ -1,5 +1,52 @@
 package lab7_radix_sorts;
 
-class RadixSorts {
+import java.util.ArrayList;
 
+public class RadixSorts {
+    public static void sort_msd(ArrayList<Integer> arr) {
+        //get max digits count
+        int max = 0;
+        for (int i : arr) {
+
+            String si = String.valueOf(i);
+            if (si.length() > max) {
+                max = si.length();
+            }
+        }
+        //now with max digits count perform alg
+        ArrayList<Integer> res_arr = new ArrayList<>();
+        sortByDigit(arr, max ,res_arr);
+
+        //fucking copy
+        arr.clear();
+        for (Integer nw : res_arr){
+            arr.add(nw);
+        }
+    }
+
+    private static void sortByDigit(ArrayList<Integer> arr, int n, ArrayList<Integer> res_arr) {
+        if (arr.size() == 0 || n == 0) {
+            return;
+        }
+        //разобраться почему некоторые элементы удваиваются (добавляются дважды)
+        if (arr.size() == 1){
+            res_arr.add(arr.get(0));
+        }
+        ArrayList<ArrayList<Integer>> digits_arrays = new ArrayList<>(10);
+        for (int i = 0; i <= 9; i++) {
+            digits_arrays.add(new ArrayList<>());
+        }
+
+        for (int c : arr) {
+            int del = (int) Math.floor(Math.pow(10.0, (double) (n - 1)));
+            int num = (c / del) % 10;
+            digits_arrays.get(num).add(c);
+        }
+        //имеем digits_arrays с отсортированными по цифрам числами.
+        for (ArrayList<Integer> subarr: digits_arrays){
+            sortByDigit(subarr, n-1, res_arr);
+            //System.out.println(subarr);
+        }
+        //System.out.println(digits_arrays);
+    }
 }
--- lab7/main.java
+++ lab7/main.java
@@ -1,9 +1,28 @@
+import java.util.ArrayList;
+import java.util.Random;
+import lab7_radix_sorts.*;
 
+class Program {
 
-class Program{
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++){
+            arr.add(rnd.nextInt(1000));
+        }
+        System.out.println(arr);
+        RadixSorts.sort_msd(arr);
+        System.out.println(arr);
     }
-
 }
+
+
+
+
+
+
+
+
+
+
+
